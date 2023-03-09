fb7a82cf8699674d70aa593da2e3ca9c7b5545fd
December <nathanielhiltonfisher@gmail.com>
2023-03-09 16:33:49 -0500

    start styling carousel

--- abc-home-renos/components/Carousel.vue
+++ abc-home-renos/components/Carousel.vue
@@ -7,10 +7,29 @@ import images from '@/images-home-carousel.json'
 </script>
 
 <template>
-  <Splide :options="{ rewind: true }" aria-label="Vue Splide Example">
+  <Splide :options="{ rewind: true, type: 'loop' }" aria-label="Vue Splide Example">
     <SplideSlide v-for="(image) in images" >
-      <img :src="'/images/home-page-carousel/' + image" >
+      <div class="image-wrapper">
+        <img :src="'/images/home-page-carousel/' + image" >
+      </div>
     </SplideSlide>
   </Splide>
 </template>
 
+<style scoped>
+.splide__slide {
+  width: auto!important;
+  margin: 16px;
+}
+.splide__slide .image-wrapper {
+  width: 405px!important;
+  height: 375px;
+  border-radius: 12px;
+  overflow: hidden;
+  background: rgba(217, 217, 217, 1);
+  box-shadow: 0px 8px 14px 0px rgba(0, 0, 0, 0.25);
+}
+.splide__slide .image-wrapper img {
+  height: 100%;
+}
+</style>
