2ce5d83a6b9d5985d1baf88bcdcf2f0e53716a2b
December <nathanielhiltonfisher@gmail.com>
2023-03-09 15:35:07 -0500

    carousel gets images from public and displays on carousel

--- abc-home-renos/components/Carousel.vue
+++ abc-home-renos/components/Carousel.vue
@@ -1,31 +1,16 @@
 <script setup lang="ts">
 import { Splide, SplideSlide } from '@splidejs/vue-splide';
-import { defineComponent } from 'vue';
 import '@splidejs/vue-splide/css';
 
-// export default defineComponent( {
-//   components: { Splide, SplideSlide },
-// } );
-
-//const config = useRuntimeConfig() 
-
 import images from '@/images-home-carousel.json'
-
-// interface ImageProps {
-//   index?: number;
-//   url?: string;
-// }
-
-// const props = defineProps<ImageProps>();
 
 </script>
 
 <template>
   <Splide :options="{ rewind: true }" aria-label="Vue Splide Example">
-    <SplideSlide 
-      v-for="(image) in images" 
-      :src="'/images/home-page-carousel/' + image" 
-    />
+    <SplideSlide v-for="(image) in images" >
+      <img :src="'/images/home-page-carousel/' + image" >
+    </SplideSlide>
   </Splide>
 </template>
 
